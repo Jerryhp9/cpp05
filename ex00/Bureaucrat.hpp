ae408173bd769f36a9033894453d66adf4130f98
jerryhp <horjarry@gmail.com>
2026-08-16 16:47:15 +0800

    designed class, to implement methods

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -2,16 +2,34 @@
 #define BUREAUCRAT_HPP
 
 #include <iostream>
+#include <exception>
 
 class Bureaucrat {
 	private:
 		const std::string name;
-		int grade;
+		unsigned int grade;
 	public: //getters for both name and grade, both exception implementation mentioned in the subject, 
 			//increment and decrement within the range 1 to 150, insertion operator overload
-		Bureaucrat(unsigned int num);
+		Bureaucrat(unsigned int num, const std::string name);
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat& operator=(const Bureaucrat& other);
 		~Bureaucrat();
-		
+
+		const std::string getName();
+		unsigned int getGrade();
+
+		Bureaucrat& incrementGrade();
+		Bureaucrat& decrementGrade();
+
+		class GradeTooHighException : public std::exception {
+			virtual const char *what()const throw();
+		};	
+
+		class GradeTooLowException : public std::exception {
+			virtual const char *what()const throw();
+		};	
 };
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& obj);
 
 #endif
