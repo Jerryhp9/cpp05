c83b4b6175800e8da9a700064536bfebc54f68b9
jerryhp <horjarry@gmail.com>
2026-08-20 12:08:41 +0800

    added ASCII Tree

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -1,5 +1,47 @@
 #include "ShrubberyCreationForm.hpp"
 
 ShrubberyCreationForm::ShrubberyCreationForm() : AForm("unknown", 145, 137) {
+	std::cout << "ShrubberyCreationForm default constructor called" << std::endl;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string target) : AForm(target, 145, 137) {
 	std::cout << "ShrubberyCreationForm constructor called" << std::endl;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other) : AForm(other) {
+	std::cout << "ShrubberyCreationForm copy constructor called" << std::endl;
+}
+
+ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm& other) {
+	if (this != &other)
+	{
+		AForm::operator=(other);
+	}
+	return (*this);
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm() {
+	std::cout << "ShrubberyCreationForm destructor called" << std::endl;
+}
+
+void ShrubberyCreationForm::execute(const Bureaucrat& executor) const {
+	if (AForm::checkSignExecute(executor) == true)
+	{
+		std::ofstream outfile;
+		outfile.open(getName() + "_Shrubbery");
+		outfile
+		<< "               ,@@@@@@@,\n"
+		<< "       ,,,.   ,@@@@@@/@@,  .oo8888o.\n"
+		<< "    ,&%%&%&&%,@@@@@/@@@@@@,8888\\88/8o\n"
+		<< "   ,%&\\%&&%&&%,@@@\\@@@/@@@88\\88888/88'\n"
+		<< "   %&&%&%&/%&&%@@\\@@/ /@@@88888\\88888'\n"
+		<< "   %&&%/ %&%%&&@@\\ V /@@' `88\\8 `/88'\n"
+		<< "   `&%\\ ` /%&'    |.|        \\ '|8'\n"
+		<< "       |o|        | |         | |\n"
+		<< "       |.|        | |         | |\n"
+		<< "    \\\\/ ._\\//_/__/  ,\\_//__\\\\/.  \\_//__/_ \n"
+		<< "\n"
+		<< "               (Art by jgs)\n";
+	}
+	
 }
--- ex02/ShrubberyCreationForm.hpp
+++ ex02/ShrubberyCreationForm.hpp
@@ -2,18 +2,16 @@
 #define SHRUBBERY
 
 #include <iostream>
+#include <fstream>
 #include "AForm.hpp"
 
 class ShrubberyCreationForm : public AForm {
-	private:
-		std::string target;
 	public:
 		ShrubberyCreationForm();
-		ShrubberyCreationForm(std::string s_target);
+		ShrubberyCreationForm(std::string target);
 		ShrubberyCreationForm(const ShrubberyCreationForm& other);
 		ShrubberyCreationForm& operator=(const ShrubberyCreationForm& other);
 		~ShrubberyCreationForm();
-		std::string getTarget();
 		void execute(const Bureaucrat& executor) const;
 };
 
