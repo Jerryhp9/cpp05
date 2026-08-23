540ea89a843c53295fdd4e4a64515e905395b14c
jerryhp <jerryhp@DESKTOP-C85HKKA.localdomain>
2026-08-23 23:20:53 +0800

    implemented Intern class methods, to test

---
+++ ex03/Intern.cpp
@@ -0,0 +1,52 @@
+#include  "Intern.hpp"
+
+Intern::Intern() {
+	std::cout  << "Intern default constructor called" << std::endl;
+}
+
+Intern::Intern(const  Intern& other)  {
+	*this = other;
+	std::cout  << "Intern copy constructor called" << std::endl;
+}
+
+Intern& Intern::operator=(const Intern& other)  {
+	std::cout << "Intern copy assignment operator called" << std::endl;
+	(void) other;
+	return (*this);
+}
+
+Intern::~Intern() {
+	std::cout  << "Intern destructor called" << std::endl;
+}
+
+formType::type hash_Form(std::string formName)
+{
+	if (formName == "ShrubberyCreationForm")
+		return (formType::ShrubberyCreationForm);
+	if (formName == "RobotomyRequestForm")
+		return (formType::RobotomyRequestForm);
+	if (formName == "PresidentialPardonForm")
+		return (formType::PresidentialPardonForm);
+	return (formType::UnknownForm);
+}
+
+AForm* Intern::makeForm(std::string formName, std::string target) {
+	AForm *form = NULL;
+	switch (hash_Form(formName)) {
+		case (formType::ShrubberyCreationForm):
+			form = new ShrubberyCreationForm(target);
+			std::cout << "Intern creates " << form->getName() << std::endl;
+			return (form);
+		case (formType::RobotomyRequestForm):
+			form = new RobotomyRequestForm(target);
+			std::cout << "Intern creates " << form->getName() << std::endl;
+			return (form);
+		case (formType::PresidentialPardonForm):
+			form = new PresidentialPardonForm(target);
+			std::cout << "Intern creates " << form->getName() << std::endl;
+			return (form);
+		case (formType::UnknownForm):
+			std::cerr << "Unknown Form name" << std::endl;
+			return (NULL);
+	}
+}
--- ex03/Intern.hpp
+++ ex03/Intern.hpp
@@ -7,13 +7,22 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 
+struct  formType {
+	enum  type {
+		ShrubberyCreationForm,
+		RobotomyRequestForm,
+		PresidentialPardonForm,
+		UnknownForm
+	};
+};
+
 class Intern : virtual public AForm, public PresidentialPardonForm, public RobotomyRequestForm, public ShrubberyCreationForm {
 	public:
 		Intern();
 		Intern(const Intern& other);
 		Intern&  operator=(const Intern& other);
 		~Intern();
-		AForm makeForm(); 
+		AForm* makeForm(std::string formName, std::string target); 
 };
 
 #endif
